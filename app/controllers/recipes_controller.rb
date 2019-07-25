0a188a0dac395bb00ec398027a184358b98463db
Trevahr Robertson <Trevahr@gmail.com>
2019-07-25 16:09:49 -0400

    working user recipe ingredient relationship

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,45 @@
 class RecipesController < ApplicationController
+
+    before_action :find_recipe, only: [:show]
+
+    def index
+    @recipes = Recipe.all
+    end
+    
+    def show
+        @user = User.find(@recipe.user_id)
+
+    end
+
+    def new
+        @recipe = Recipe.new()
+        @recipe.ingredients.build()
+        @recipe.ingredients.build()
+    end
+
+    def create
+
+        @recipe = Recipe.new(recipe_params)
+
+        if @recipe.valid?
+            @recipe.save
+            redirect_to recipe_path(@recipe)
+        else
+            render :new
+        end
+
+    end
+
+
+
+private
+
+def recipe_params
+    params.require(:recipe).permit(:name, :user_id, ingredients_attributes: [:name])
+end
+
+def find_recipe
+  @recipe =  Recipe.find(params[:id])
+end
+
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,5 +2,6 @@ class Recipe < ApplicationRecord
     belongs_to :user
     has_many :recipe_ingredients
     has_many :ingredients, through: :recipe_ingredients
+    accepts_nested_attributes_for :ingredients
     validates :name, presence: true
 end
